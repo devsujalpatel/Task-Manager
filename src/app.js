0eaae6f7456f541ae81865fc6e7e0fdc410c536a
devsujalpatel <sujal7455@gmail.com>
2025-04-03 01:57:52 +0530

    few changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+
 
 app.use("/api/v1/", healthCheckRouter)
+app.use("/api/v1/", authRoutes)
 
 export default app;
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,8 +1,13 @@
 import { asyncHandler } from "../utils/async-handler.js";
+import {User} from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js"
+import { ApiResponse } from "../utils/api-response.js"
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const {email, username, password} = req.body;
-    registrationValidation(body)
+    
 });
 
 export {registerUser}
